90ac6df1ab70d4010f28f768003d6d12ff1c975e
Adriana Guevara <adrianaymary@hotmail.com>
2026-06-28 10:37:31 -0400

    Fix Main.java to wire services into MainFrame

--- D2/ibank/src/main/java/ca/concordia/Main.java
+++ D2/ibank/src/main/java/ca/concordia/Main.java
@@ -1,10 +1,8 @@
 package ca.concordia;
 
 import ca.concordia.data.UserDatabase;
-import ca.concordia.model.AccountType;
-import ca.concordia.model.Session;
-import ca.concordia.model.TransactionResult;
-import ca.concordia.model.User;
+import ca.concordia.data.demo;
+import ca.concordia.service.AuthenticationService;
 import ca.concordia.service.BankingService;
 import ca.concordia.ui.MainFrame;
 
@@ -12,8 +10,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        UserDatabase db = demo.createDemoDatabase();
+        AuthenticationService authService = new AuthenticationService(db);
+        BankingService bankingService = new BankingService(db);
+
         SwingUtilities.invokeLater(() -> {
-            MainFrame frame = new MainFrame();
+            MainFrame frame = new MainFrame(authService, bankingService);
             frame.setVisible(true);
         });
     }
